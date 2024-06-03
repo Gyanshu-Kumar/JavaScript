17fe31ab140994dde770bb135c93673289dee1c3
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-06-03 20:45:21 +0530

    Added operator

---
+++ Operations.js
@@ -0,0 +1,53 @@
+/**
+ * Assignment operators
+ * =, +=, -=, *=, /=, %=, **=
+ * var a = 10;
+*/
+
+// Airthmetic operators
+// +, -, *, /, %, ++, --
+// console.log(10 + 5);
+// console.log(10 - 5);
+// console.log(10 * 5);
+// console.log(4**2); // 4^2 = 16 Power of
+// console.log(10 / 5);
+// console.log(10 % 5); // Remainder
+
+//increment
+// var a = 10;
+// a = a + 1;
+// console.log(a); // 11
+
+// var a = 10; // Post-increment
+// console.log(a++); // 10
+// console.log(a); // 11 if you want to see the increment run again
+
+// var a = 10; // Pre-increment
+// console.log(++a); // 11 // First increment then print
+
+// decrement
+// var a = 10;
+// a--;
+// console.log(a); // 9
+
+
+
+// Comparison operators or Relational operators 
+// ==, ===, !=, !==, >, <, >=, <=
+// console.log(10 == 10); // true
+// console.log(10 == '10'); // true // because it only checks the value
+// console.log(10 === '10'); // false // because it checks the value and datatype
+// console.log(10 != 10); // false
+// console.log(10 !== '10'); // true
+// console.log(10 > 10); // false
+// console.log(10 < 10); // false
+// console.log(10 >= 10); // true
+
+
+// Logical operators
+// &&, ||, !
+// console.log(true && true);
+
+// Bitwise operators
+// &, |, ^, ~, <<, >>
+// console.log(10 & 5); Output: 0 
