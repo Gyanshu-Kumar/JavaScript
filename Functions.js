a919f981c79b57e37578cf9b1237a96e9f94c1dd
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-06-11 11:31:51 +0530

    Added Function JS

---
+++ Functions.js
@@ -0,0 +1,26 @@
+// Q - What is Function in JavaScript?
+// Ans - Function is a block of code that can be defined once and can be executed or invoked multiple times.
+ 
+// function helloStudent() { // Function Declaration
+//   console.log("Hello Student"); // Function Body
+// } // Function Definition
+
+// helloStudent(); // Hello Student // Function Invocation
+
+// Naming Convention for Function
+// Function name should be camelCase
+// Function name should be meaningful
+// Function name should be a verb
+// Function name should be in lowercase
+// There could be no space in the function name
+
+// function sum(a, b) { // Function Declaration
+//   return a + b; // Function Body
+// } // Function Definition
+// console.log(sum(10, 20)); // 30 // Function Invocation
+
+// function func(){
+//     console.log(arguments);
+// }
+// func(1, 2, 3, 4, 5); // Arguments(5) [1, 2, 3, 4, 5, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+
---
+++ arrow_functions.js
@@ -0,0 +1,26 @@
+// Q - What is Arrow Function in JavaScript?
+// Ans - Arrow Function is a short-hand syntax for writing function expressions. It is also known as Fat Arrow Function.
+
+// -> Function with no arguments and no return type
+// var hello = () => console.log("Hello World"); // Arrow Function
+// hello(); // Hello World
+
+// -> Function with parameters and return type
+// var sum = (a, b) => a + b; // Arrow Function
+// console.log(sum(10, 20)); // 30
+
+
+// -> Function with parameters, multiple lines of body
+// var process = (a, b) => {
+//   console.log("Hello World");
+//   return a + b;
+// }
+// console.log(process(10, 20)); // Hello World 30
+
+
+// What is IIFE in JavaScript?
+// IIFE - Immediately Invoked Function Expression
+// (function() {  // The function which has no name is called Anonymous Function
+//   console.log("Hello World");
+// })();
+
