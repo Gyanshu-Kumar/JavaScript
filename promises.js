b4ccf355648758c81076e8e5476d7480e0153b4f
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-07-14 13:46:41 +0530

    Added Express

--- promises.js
+++ promises.js
@@ -74,48 +74,50 @@ Promise.all([f1Promise, f2Promise, f3Promise]).then((messages) => {
     console.log("All Friends are not in for Goa")
 }) **/
 
-const gf1Promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (Math.random() > 0.5) {
-      resolve("GF1 : I Will Come for dinner");
-    } else {
-      reject("GF1 : Phone is Unreachable");
-    }
-  }, 1000);
-});
+    
 
-const gf2Promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (Math.random() > 0.5) {
-      resolve("GF2 : I Will Come for dinner");
-    } else {
-      reject("GF2 : Phone is Unreachable");
-    }
-  }, 2000);
-});
-const gf3Promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (Math.random() > 0.5) {
-      resolve("GF3 : I Will Come for dinner");
-    } else {
-      reject("GF3 : Phone is Unreachable");
-    }
-  }, 3000);
-});
-Promise.any([gf1Promise, gf2Promise, gf3Promise])
-  .then((messages) => {
-    console.log(messages);
-    console.log("All GF are in for dinner");
-  })
-  .catch((messages) => {
-    console.log(messages);
-    console.log("All GF are not in for dinner");
-  });
+// const gf1Promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (Math.random() > 0.5) {
+//       resolve("GF1 : I Will Come for dinner");
+//     } else {
+//       reject("GF1 : Phone is Unreachable");
+//     }
+//   }, 1000);
+// });
 
-// Difference between promise.all and promise.any -> Promise.all() is a promise that resolves when all of the promises in the iterable argument have resolved, or rejects with the reason of the first promise that rejects.
-//Promise.any() is a promise that resolves as soon as any of the promises in the iterable argument resolves, or rejects if all of the promises reject.
-//If the returned promise resolves, it is resolved with the value of the first promise in the iterable that resolved.
-//If all of the promises are rejected,
-//Promise.any is rejected with an AggregateError, a new subclass of Error that groups together individual errors.
+// const gf2Promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (Math.random() > 0.5) {
+//       resolve("GF2 : I Will Come for dinner");
+//     } else {
+//       reject("GF2 : Phone is Unreachable");
+//     }
+//   }, 2000);
+// });
+// const gf3Promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (Math.random() > 0.5) {
+//       resolve("GF3 : I Will Come for dinner");
+//     } else {
+//       reject("GF3 : Phone is Unreachable");
+//     }
+//   }, 3000);
+// });
+// Promise.any([gf1Promise, gf2Promise, gf3Promise])
+//   .then((messages) => {
+//     console.log(messages);
+//     console.log("All GF are in for dinner");
+//   })
+//   .catch((messages) => {
+//     console.log(messages);
+//     console.log("All GF are not in for dinner");
+//   });
 
-// In Promise.all if any anyone reject then all go to catch block but in Promise.any if any one resolve then all go to then block
+// // Difference between promise.all and promise.any -> Promise.all() is a promise that resolves when all of the promises in the iterable argument have resolved, or rejects with the reason of the first promise that rejects.
+// //Promise.any() is a promise that resolves as soon as any of the promises in the iterable argument resolves, or rejects if all of the promises reject.
+// //If the returned promise resolves, it is resolved with the value of the first promise in the iterable that resolved.
+// //If all of the promises are rejected,
+// //Promise.any is rejected with an AggregateError, a new subclass of Error that groups together individual errors.
+
+// // In Promise.all if any anyone reject then all go to catch block but in Promise.any if any one resolve then all go to then block
